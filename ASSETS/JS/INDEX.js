21b6a606445adbed233da1e5fef86aa404dd40bd
Mike-mk3 <mike666mtz@gmail.com>
2023-11-11 15:43:16 -0600

    CONECCION CON API
    Agregue la URL y cree el vinculo para que los resultados se vean reflejados en la grafica

--- ASSETS/JS/INDEX.js
+++ ASSETS/JS/INDEX.js
@@ -1,1 +1,35 @@
 console.log ("ENTRO A MI ARCHIVO JS");
+
+
+
+
+
+fetch ("https://dev4humans.com.mx/api/Clases/ventas_juguetes")
+.then (Response => Response.json())
+.then (datosApi => {
+    console.log (datosApi);
+    const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: datosApi.data.labels,
+        datasets: [{
+            label: '# of Votes',
+            data: datosApi.data.data,
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+})
+
+
+
+
